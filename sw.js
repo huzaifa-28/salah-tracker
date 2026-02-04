3bcf48e43d6b01d2f6648948baaff8bd8603ef7d
shancollectives <huzaifaaa.028@gmail.com>
2026-02-04 18:32:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'salah-v5';
+const CACHE_NAME = 'salah-v11';
+// We only cache the root and index. If these work, the white screen disappears.
 const ASSETS = [
   './',
   'index.html'
@@ -7,8 +8,6 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      // If index.html is missing, this will fail. 
-      // Ensure the file is named exactly index.html!
       return cache.addAll(ASSETS);
     })
   );
@@ -16,12 +15,21 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    // This deletes OLD caches so they don't conflict
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Return cached file OR go to network
       return response || fetch(event.request);
     })
   );
